3848960a4d7834c06b5e2520d728553816c315a1
pascalinelauriol85 <pascalinelauriol85@gmail.com>
2021-04-21 15:33:51 +0200

    twitter

---
+++ lib/live.rb
@@ -0,0 +1,27 @@
+ # ligne très importante qui appelle la gem.
+ require 'twitter'
+ require 'dotenv'
+
+ # n'oublie pas les lignes pour Dotenv ici…
+ Dotenv.load('.env')
+ # quelques lignes qui appellent les clés d'API de ton fichier .env
+ client = Twitter::Streaming::Client.new do |config|
+   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
+ 
+ client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  
+ 
+ ##############################################################
+ # trouvé des personne qui ont tweeter et les suivre
+ client.sample do |object|
+ object.text if object.is_a?(Twitter::tweet)
+ end
